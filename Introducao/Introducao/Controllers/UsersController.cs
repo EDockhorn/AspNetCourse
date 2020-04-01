69926e90a56e5590565547714eaf07bb0bde2c7c
EDockhorn <johnnyerick111@hotmail.com>
2020-04-01 18:58:38 -0300

    Validações

---
+++ Introducao/Introducao/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Usuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Usuario(User user)
+        {
+            if (string.IsNullOrEmpty(user.Name))
+                ModelState.AddModelError("Name", "Nome não foi preenchido");
+
+            if (user.Password != user.ConfirmPassword)
+                ModelState.AddModelError("", "As senhas são diferentes");
+
+            return ModelState.IsValid ? View("Resultado", user) : View();
+        }
+
+        public ActionResult Resultado(User user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ Introducao/Introducao/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Introducao.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int YearsOld { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Observations { get; set; }
+
+        //public User()
+        //{
+        //    Name = "Johnny Erick Dockhorn";
+        //    YearsOld = 20;
+        //    Login = "johnnydock";
+        //    Email = "johnnyerick111@hotmail.com";
+        //    Password = "123131";
+        //    ConfirmPassword = "12121";
+        //    Observations = "Observações Qualquer";
+        //}
+    }
+}
